0cdd7e35a456975c93881f7c4684c13b759b091b
IsStephy <145896071+IsStephy@users.noreply.github.com>
2024-10-23 18:09:04 +0300

    Task 2 Lab 2, at run takes the arguments path to the .txt file and the name of the file

--- oop-lab_1/src/main/java/oop/practice/Main.java
+++ oop-lab_1/src/main/java/oop/practice/Main.java
@@ -1,46 +1,18 @@
 package oop.practice;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(args[0] + " " + args[1] + " " + args[2]);
+        System.out.println(args[0]);
+        FileReader readFile = new FileReader();
+        String path = args[0];
+        String fileContent = readFile.readFileIntoString(path);
+        TextData text = new TextData(fileContent);
 
-
-        Display display1 = new Display(1920, 1080, 401.5f, "Model A");
-        Display display2 = new Display(2560, 1440, 530.3f, "Model B");
-        Display display3 = new Display(1920, 1080, 401.5f, "Model C");
-        List<Display> displayList = new ArrayList<>();
-        displayList.add(display1);
-        displayList.add(display2);
-        displayList.add(display3);
-
-
-        int a = 0;
-        for (int i = 0; i < displayList.size(); i++) {
-            for (int j = i + 1; j < displayList.size(); j++) {
-                Display displayA = displayList.get(i);
-                Display displayB = displayList.get(j);
-
-
-                System.out.println("Comparing " + displayA.getModel() + " with " + displayB.getModel());
-
-
-                if (args[a].equals("0")) {
-                    displayA.compareSize(displayB);
-                }
-                if (args[a].equals("1")) {
-                    displayA.compareSharpness(displayB);
-                }
-                if (args[a].equals("2")) {
-                    displayA.compareWithMonitor(displayB);
-                }
-                a += 1;
-                System.out.println();
-            }
-        }
+        System.out.println("Number of Vowels: " + text.getNumberOfVowels());
+        System.out.println("Number of Consonants: " + text.getNumberOfConsonants());
+        System.out.println("Number of Letters: " + text.getNumberOfLetters());
+        System.out.println("Number of Sentences: " + text.getNumberOfSentences());
+        System.out.println("Longest Word: " + text.getLongestWord());
     }
 }
