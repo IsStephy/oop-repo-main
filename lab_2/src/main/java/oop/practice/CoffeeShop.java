a0c311e97d0e331dc22ffc33176e9d260dabd544
IsStephy <145896071+IsStephy@users.noreply.github.com>
2024-11-05 20:30:46 +0200

    Forth Task: Implement encapsulation in coffee-making application: restrict access to Coffee class and allow Barista to handle coffee creation

--- lab_2/src/main/java/oop/practice/CoffeeShop.java
+++ lab_2/src/main/java/oop/practice/CoffeeShop.java
@@ -1,3 +1,5 @@
+package oop.practice;
+
 enum Intensity {
     LIGHT, NORMAL, STRONG
 }
@@ -48,7 +50,16 @@ class Cappuccino extends Coffee {
     public void printCoffeeDetails() {
         System.out.println("Coffee Type: " + coffee + ", Intensity: " + coffeeIntensity + ", Milk: " + mlOfMilk + "ml");
     }
+
+    public final Cappuccino makeCappuccino() {
+        System.out.println("Making " + coffee + ":");
+        System.out.println("1. Brew espresso.");
+        System.out.println("2. Steam milk.");
+        System.out.println("3. Combine espresso with steamed milk.");
+        return this; // Return the current instance
     }
+}
+
 class Americano extends Coffee {
     private int mlOfWater;
     protected final String coffeeName = "Americano";
@@ -65,9 +76,17 @@ class Americano extends Coffee {
     public String getCoffeeName() {
         return coffeeName;
     }
+
     @Override
     public void printCoffeeDetails() {
         System.out.println("Coffee Type: " + coffeeName + ", Intensity: " + coffeeIntensity + ", Water: " + mlOfWater + "ml");
+    }
+
+    public final Americano makeAmericano() {
+        System.out.println("Making " + coffeeName + ":");
+        System.out.println("1. Brew espresso.");
+        System.out.println("2. Add " + mlOfWater + "ml of hot water.");
+        return this; // Return the current instance
     }
 }
 
@@ -92,6 +111,14 @@ class SyrupCappuccino extends Cappuccino {
     @Override
     public void printCoffeeDetails() {
         System.out.println("Coffee Type: " + coffee + ", Intensity: " + coffeeIntensity + ", Milk: " + getMlOfMilk() + "ml, Syrup: " + syrup);
+    }
+
+    public final SyrupCappuccino makeSyrupCappuccino() {
+        System.out.println("Making " + coffee + ":");
+        System.out.println("1. Brew espresso.");
+        System.out.println("2. Steam milk.");
+        System.out.println("3. Combine espresso with steamed milk and add syrup: " + syrup);
+        return this; // Return the current instance
     }
 }
 
@@ -118,10 +145,56 @@ class PumpkinSpiceLatte extends Coffee {
     public void printCoffeeDetails() {
         System.out.println("Coffee Type: " + coffee + ", Intensity: " + coffeeIntensity + ", Milk: " + mlOfMilk + "ml, Pumpkin Spice: " + mgOfPumpkinSpice + "mg");
     }
+
+    public final PumpkinSpiceLatte makePumpkinSpiceLatte() {
+        System.out.println("Making " + coffee + ":");
+        System.out.println("1. Brew espresso.");
+        System.out.println("2. Add steamed milk.");
+        System.out.println("3. Add " + mgOfPumpkinSpice + "mg of pumpkin spice.");
+        return this; // Return the current instance
+    }
 }
 
+// Barista class handles coffee creation and encapsulates the Coffee class
+class Barista {
+    public void makeCoffees(Intensity intensity, String coffeeType, int mlOfMilk, int mlOfWater, SyrupType syrup, int mgOfPumpkinSpice) {
+        switch (coffeeType.toLowerCase()) {
+            case "cappuccino":
+                Cappuccino cappuccino = new Cappuccino(intensity, mlOfMilk);
+                cappuccino.makeCappuccino();
+                cappuccino.printCoffeeDetails();
+                break;
+            case "americano":
+                Americano americano = new Americano(intensity, mlOfWater);
+                americano.makeAmericano();
+                americano.printCoffeeDetails();
+                break;
+            case "syrupcappuccino":
+                SyrupCappuccino syrupCappuccino = new SyrupCappuccino(intensity, mlOfMilk, syrup);
+                syrupCappuccino.makeSyrupCappuccino();
+                syrupCappuccino.printCoffeeDetails();
+                break;
+            case "pumpkinspicelatte":
+                PumpkinSpiceLatte pumpkinSpiceLatte = new PumpkinSpiceLatte(intensity, mlOfMilk, mgOfPumpkinSpice);
+                pumpkinSpiceLatte.makePumpkinSpiceLatte();
+                pumpkinSpiceLatte.printCoffeeDetails();
+                break;
+            default:
+                System.out.println("Unknown coffee type: " + coffeeType);
+                break;
+        }
+    }
+}
+
+// Main class to run the application
 public class CoffeeShop {
     public static void main(String[] args) {
+        Barista barista = new Barista();
 
+        // Making different types of coffee through Barista
+        barista.makeCoffees(Intensity.NORMAL, "cappuccino", 120, 0, null, 0); // Making a Cappuccino
+        barista.makeCoffees(Intensity.STRONG, "americano", 0, 200, null, 0); // Making an Americano
+        barista.makeCoffees(Intensity.LIGHT, "syrupcappuccino", 150, 0, SyrupType.VANILLA, 0); // Making a Syrup Cappuccino
+        barista.makeCoffees(Intensity.NORMAL, "pumpkinspicelatte", 100, 0, null, 50); // Making a Pumpkin Spice Latte
     }
 }
